0c48d8723a1ba6ee0ab45d0513399482628ac5f6
frostkim <frostkim21@gmail.com>
2022-11-24 15:26:53 +0900

    first commit

---
+++ src/main/java/com/springboot/chunjae/ChunjaeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.chunjae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChunjaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChunjaeApplication.class, args);
+	}
+
+}
